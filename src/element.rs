7b3f03edf7693659afdb82e2a16ee89b89224961
John Teske <john.teske@protonmail.com>
2021-07-07 21:21:56 -0700

    Use BoundingBox trait with x/y/w/h

--- src/element.rs
+++ src/element.rs
@@ -1,98 +1,64 @@
-//Shape elements
-//<circle>, <ellipse>, <line>, <mesh>, <path>, <polygon>, <polyline>, <rect>
+// Shape elements
+// <circle>, <ellipse>, <line>, <mesh>, <path>, <polygon>, <polyline>, <rect>
 
 use super::units::Number;
-use svg::node::element::{Circle as _Circle, Group as _Group};
+use svg::node::element::Circle as _Circle;
 
-trait Dimensions {
-    fn width(&self) -> Number {
-        unimplemented!()
-    }
-    fn height(&self) -> Number {
-        unimplemented!()
-    }
-}
-
-struct Circle(_Circle);
-
-impl Dimensions for Circle {
-    fn width(&self) -> Number {
-        let attrs = &self.0.get_inner().get_attributes();
-        let r: Number = attrs["r"].parse().unwrap();
-        r * 2.0
-    }
-
-    fn height(&self) -> Number {
-        self.width()
-    }
-}
-
-impl From<_Circle> for Circle {
-    fn from(item: _Circle) -> Self {
-        Circle(item)
-    }
+trait BoundingBox {
+    fn x(&self) -> Number;
+    fn y(&self) -> Number;
+    fn width(&self) -> Number;
+    fn height(&self) -> Number;
 }
 
 #[derive(Default)]
-struct Circle2 {
+struct Circle {
     r: Number,
     cx: Number,
     cy: Number,
 }
 
-impl Circle2 {
+impl Circle {
     fn new() -> Self {
-        Circle2::default()
+        Circle::default()
     }
-    fn radius(mut self, r: Number) -> Self {
+    fn set_radius(mut self, r: Number) -> Self {
         self.r = r;
         self
     }
-    //
+}
+
+impl BoundingBox for Circle {
     fn x(&self) -> Number {
         &self.cx - &self.r
     }
-}
-
-impl Dimensions for Circle2 {
+    fn y(&self) -> Number {
+        &self.cy - &self.r
+    }
     fn width(&self) -> Number {
         &self.r * 2.0
     }
-
     fn height(&self) -> Number {
         self.width()
     }
 }
 
-impl From<_Circle> for Circle2 {
-    fn from(item: _Circle) -> Self {
-        let attrs = &item.get_inner().get_attributes();
-        let r: Number = attrs["r"].parse().unwrap();
-        let cx: Number = attrs["r"].parse().unwrap();
-        let cy: Number = attrs["r"].parse().unwrap();
-        Circle2 { r, cx, cy }
-    }
-}
+// impl From<_Circle> for Circle {
+//     fn from(item: _Circle) -> Self {
+//         let attrs = &item.get_inner().get_attributes();
+//         let r: Number = attrs["r"].parse().unwrap();
+//         let cx: Number = attrs["cx"].parse().unwrap();
+//         let cy: Number = attrs["cy"].parse().unwrap();
+//         Circle { r, cx, cy }
+//     }
+// }
 
-struct Group(_Group);
-
-impl Group {}
-
-impl Dimensions for Group {
-    fn width(&self) -> Number {
-        let attrs = &self.0.get_inner().get_attributes();
-        let r: Number = attrs["r"].parse().unwrap();
-        r * 2.0
-    }
-
-    fn height(&self) -> Number {
-        self.width()
-    }
-}
-
-impl From<_Group> for Group {
-    fn from(item: _Group) -> Self {
-        Group(item)
+impl Into<_Circle> for Circle {
+    fn into(self) -> _Circle {
+        _Circle::new()
+            .set("r", self.r)
+            .set("cx", self.cx)
+            .set("cy", self.cy)
     }
 }
 
@@ -101,20 +67,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn circle_dimensions() {
-        let source = _Circle::new().set("r", 3);
-        let with_dimensions = Circle::from(source);
+    fn circle() {
+        let c = Circle::new().set_radius(3.);
 
-        assert_eq!(with_dimensions.width(), 6.0);
-        assert_eq!(with_dimensions.height(), 6.0);
-    }
+        assert_eq!(c.width(), 6.0);
+        assert_eq!(c.height(), 6.0);
+        assert_eq!(c.x(), -3.0);
 
-    #[test]
-    fn circle2() {
-        let c2 = Circle2::new().radius(3.);
-
-        assert_eq!(c2.width(), 6.0);
-        assert_eq!(c2.height(), 6.0);
-        assert_eq!(c2.x(), -3.0);
+        let c: _Circle = c.into();
+        assert_eq!(c.to_string(), "circ");
     }
 }
