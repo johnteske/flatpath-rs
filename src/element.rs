d8e104ad5c0bb6477fa842d1724261caa81d4b53
John Teske <john.teske@protonmail.com>
2021-07-07 18:22:51 -0700

    Implement From for element, not 'new' method

--- src/element.rs
+++ src/element.rs
@@ -12,12 +12,6 @@ trait Dimensions {
 
 struct Circle(_Circle);
 
-impl Circle {
-    fn new(source: _Circle) -> Circle {
-        Circle(source)
-    }
-}
-
 impl Dimensions for Circle {
     fn width(&self) -> Number {
         let attrs = &self.0.get_inner().get_attributes();
@@ -30,6 +24,12 @@ impl Dimensions for Circle {
     }
 }
 
+impl From<_Circle> for Circle {
+    fn from(item: _Circle) -> Self {
+        Circle(item)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -37,7 +37,7 @@ mod tests {
     #[test]
     fn circle_dimensions() {
         let source = _Circle::new().set("r", 3);
-        let with_dimensions = Circle::new(source);
+        let with_dimensions = Circle::from(source);
 
         assert_eq!(with_dimensions.width(), 6.0);
         assert_eq!(with_dimensions.height(), 6.0);
