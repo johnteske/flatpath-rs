1ddf50a1e4b77aa51367cb3060c6a2178a67b5f4
John Teske <john.teske@protonmail.com>
2021-07-25 17:11:07 -0700

    Base plant ladder rung depth off side width

--- src/projects/plant_ladder.rs
+++ src/projects/plant_ladder.rs
@@ -6,19 +6,24 @@ use crate::units::Number;
 use crate::units::{inches, mm};
 
 pub fn project() -> Document {
+    // parameters
+    let t = mm(3.);
+    let side_width = t * 4.0;
+    let num_rungs = 6;
+
+    // constants and derived values
     let rung_width = inches(2.);
     let rung_spacing = inches(1.5);
-    let num_rungs = 6;
-    let t = mm(3.);
-    let corner_radius = mm(1.0); // soften the outer edge a little
+    let side_height = (num_rungs as Number * t) + ((num_rungs as Number + 1.) * rung_spacing);
+    let rung_depth = side_width - (t * 2.0);
+    let rung_tab_depth = rung_depth / 2.0;
+    let corner_radius = mm(1.0); // soften the outer edges a little
 
     let mut g = Group::new()
         .set("fill", "none")
         .set("stroke", "black")
         .set("stroke-width", 1);
 
-    let side_width = t * 4.0;
-    let side_height = (num_rungs as Number * t) + ((num_rungs as Number + 1.) * rung_spacing);
     let mut side = Group::new().add(
         Rectangle::new()
             .set("width", side_width)
@@ -30,9 +35,9 @@ pub fn project() -> Document {
     for i in 0..num_rungs {
         side = side.add(
             Rectangle::new()
-                .set("width", t)
+                .set("width", rung_tab_depth)
                 .set("height", t)
-                .set("x", t * 1.5)
+                .set("x", (side_width - rung_tab_depth) / 2.0)
                 .set("y", (rung_spacing * (i as Number + 1.)) + (t * i as Number)),
         );
     }
@@ -43,19 +48,19 @@ pub fn project() -> Document {
         // top edge
         .add((t, 0.))
         .add((t + rung_width, 0.))
-        .add((t + rung_width, t * 0.5))
+        .add((t + rung_width, rung_depth * 0.25))
         // right tab
-        .add_r((t + rung_width + t, t * 0.5), corner_radius)
-        .add_r((t + rung_width + t, t * 1.5), corner_radius)
-        .add((t + rung_width, t * 1.5))
+        .add_r((t + rung_width + t, rung_depth * 0.25), corner_radius)
+        .add_r((t + rung_width + t, rung_depth * 0.75), corner_radius)
+        .add((t + rung_width, rung_depth * 0.75))
         // bottom edge
-        .add((t + rung_width, t * 2.0))
-        .add((t, t * 2.0))
-        .add((t, t * 1.5))
+        .add((t + rung_width, rung_depth))
+        .add((t, rung_depth))
+        .add((t, rung_depth * 0.75))
         // left tab
-        .add_r((0., t * 1.5), corner_radius)
-        .add_r((0., t * 0.5), corner_radius)
-        .add((t, t * 0.5))
+        .add_r((0., rung_depth * 0.75), corner_radius)
+        .add_r((0., rung_depth * 0.25), corner_radius)
+        .add((t, rung_depth * 0.25))
         .close();
 
     g = g.add(Path::new().set("d", rung_data).set(
