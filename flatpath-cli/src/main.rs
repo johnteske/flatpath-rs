eb5e93a1198b610c26378d998b5c7125418b3fcf
John Teske <john.teske@protonmail.com>
2021-10-09 00:17:20 -0700

    Add list subcommand and project flag

--- flatpath-cli/src/main.rs
+++ flatpath-cli/src/main.rs
@@ -1,6 +1,34 @@
+use clap::{App, Arg, SubCommand};
+
 fn main() {
     let projects = projects::projects();
-    // TODO choose
-    let p = projects.get("plant-ladder").unwrap();
-    projects::save(&**p).expect("error saving");
+
+    let matches = App::new("flatpath-cli")
+        .arg(
+            Arg::with_name("project")
+                .short("p")
+                .long("project")
+                .value_name("NAME")
+                .help("PROject")
+                .takes_value(true),
+        )
+        .subcommand(SubCommand::with_name("list").about("list projects"))
+        .get_matches();
+
+    if matches.subcommand_matches("list").is_some() {
+        for p in projects.keys() {
+            println!("{}", p);
+        }
+        return;
+    }
+
+    match matches.value_of("project") {
+        Some(project) => {
+            let p = projects.get(&project).unwrap();
+            projects::save(&**p).expect("error saving");
+        }
+        None => {
+            println!("{}", matches.usage());
+        }
+    }
 }
--- projects/src/lib.rs
+++ projects/src/lib.rs
@@ -1,6 +1,5 @@
 use std::collections::HashMap;
 use std::io;
-use svg;
 
 mod plant_ladder;
 mod quefrency_60_case;
@@ -9,17 +8,20 @@ pub trait Project {
     fn generate(&self) -> svg::Document;
 }
 
-type Projects<'a> = HashMap<&'a str, Box::<dyn Project + 'static>>;
+type Projects<'a> = HashMap<&'a str, Box<dyn Project + 'static>>;
 
 pub fn projects<'a>() -> Projects<'a> {
     let mut projects: Projects<'a> = HashMap::new();
     projects.insert("plant-ladder", Box::new(plant_ladder::PlantLadder));
-    projects.insert("quefrency-60-case", Box::new(quefrency_60_case::KeyboardCase));
+    projects.insert(
+        "quefrency-60-case",
+        Box::new(quefrency_60_case::KeyboardCase),
+    );
     projects
 }
 
 // TODO output path
 pub fn save(project: &dyn Project) -> io::Result<()> {
-    let doc = project.generate(); 
+    let doc = project.generate();
     svg::save("image.svg", &doc)
 }
